6b09fffb61f76b72ffa0702f1832402cef91c945
Christhopher Lion <christhopherleao@icloud.com>
2023-01-05 10:43:10 +0100

    Bridging headers
    Signed-off-by: Christhopher Lion <christhopherleao@icloud.com>

---
+++ ios/SwiftUIView.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIView.swift
+//  Kantora
+//
+//  Created by Chris.Lion on 05/01/2023.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIView()
+    }
+}
